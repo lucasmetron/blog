6e43e04c87f1767a8340973f40a75da570ed73d6
Lucas Rosa <lucasmetron@gmail.com>
2025-08-01 18:07:48 -0300

    teste

--- src/components/MainPost/index.tsx
+++ src/components/MainPost/index.tsx
@@ -1,6 +1,9 @@
-import { PostType } from '@/Types/PostType';
 import Image from 'next/image';
-import React from 'react';
+import clsx from 'clsx';
+
+import { colors } from '@/styles/pallete';
+import { PostType } from '@/Types/PostType';
+import { formatDate } from '@/utils/functions';
 
 interface MainPostType {
   post: PostType;
@@ -8,20 +11,35 @@ interface MainPostType {
 
 const MainPost = ({ post }: MainPostType) => {
   return (
-    <div className='relative w-[100%] h-[30vh] grid grid-cols-[1fr_1fr] bg-amber-300 gap-[1.5rem]'>
+    <div
+      className={clsx(
+        'relative',
+        'w-[100%]',
+        'h-[35vh]',
+        'max-[800]:h-fit',
+        'grid',
+        'grid-cols-[0.7fr_1fr]',
+        'max-[800]:grid-cols-[1fr]',
+        'max-[800]:grid-rows-[2fr_1fr]',
+        'gap-[1.5rem]',
+        'mt-[2rem]',
+        'cursor-pointer',
+      )}
+    >
       <div className='relative w-full h-full'>
         <Image
           src={post.coverImageUrl}
           alt={post.slug}
           fill
           className='object-fill rounded-[5px]'
-          radioGroup='50'
         />
       </div>
-      <div>
-        <p>{post.createdAt}</p>
-        <h2>{post.title}</h2>
-        <p>{post.excerpt}</p>
+      <div className='flex flex-col items-start justify-center gap-[1rem]'>
+        <p style={{ color: colors.label }}>{formatDate(post.createdAt)}</p>
+        <h2 style={{ color: colors.textBold }} className='font-bold text-3xl'>
+          {post.title}
+        </h2>
+        <p style={{ color: colors.textRegular }}>{post.excerpt}</p>
       </div>
     </div>
   );
---
+++ src/utils/functions.ts
@@ -0,0 +1,18 @@
+export function formatDate(
+  isoString: string,
+  includeTime: boolean = true,
+): string {
+  const date = new Date(isoString);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // meses começam do zero
+  const year = date.getFullYear();
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  const formattedDate = `${day}/${month}/${year}`;
+  const formattedTime = `${hours}:${minutes}`;
+
+  return includeTime ? `${formattedDate} ${formattedTime}` : formattedDate;
+}
