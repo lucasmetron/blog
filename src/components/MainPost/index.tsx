78ea22b8634cbaa07ecd00914bcbddf492c73242
Lucas Rosa <lucasmetron@gmail.com>
2025-08-06 16:12:17 -0300

    teste

--- src/components/MainPost/index.tsx
+++ src/components/MainPost/index.tsx
@@ -11,35 +11,34 @@ interface MainPostType {
 
 const MainPost = ({ post }: MainPostType) => {
   return post !== undefined ? (
-    <Link href='/about'>
-      <div
-        className={clsx(
-          'relative',
-          'w-[100%]',
-          'h-[40vh]',
-          'max-[800]:h-fit',
-          'grid',
-          'grid-cols-[0.7fr_1fr]',
-          'max-[800]:grid-cols-[1fr]',
-          'max-[800]:grid-rows-[2fr_1fr]',
-          'gap-[1.5rem]',
-          'mt-[2rem]',
-          'cursor-pointer',
-        )}
-      >
-        <div className='relative w-full h-full'>
-          <Image
-            src={post.coverImageUrl}
-            alt={post.slug}
-            fill
-            className='object-fill rounded-[5px]'
-          />
-        </div>
-        <div className='flex flex-col items-start justify-center gap-[1rem]'>
-          <p>{formatDate(post.createdAt)}</p>
-          <h2 className='font-bold text-3xl'>{post.title}</h2>
-          <p className='opacity-90'>{post.excerpt}</p>
-        </div>
+    <Link
+      href='/about'
+      className={clsx(
+        'relative',
+        'w-[100%]',
+        'h-[40vh]',
+        'max-[800]:h-fit',
+        'grid',
+        'grid-cols-[0.7fr_1fr]',
+        'max-[800]:grid-cols-[1fr]',
+        'max-[800]:grid-rows-[2fr_1fr]',
+        'gap-[1.5rem]',
+        'mt-[2rem]',
+        'cursor-pointer',
+      )}
+    >
+      <div className='relative w-full h-full'>
+        <Image
+          src={post.coverImageUrl}
+          alt={post.slug}
+          fill
+          className='object-fill rounded-[5px]'
+        />
+      </div>
+      <div className='flex flex-col items-start justify-center gap-[1rem]'>
+        <p>{formatDate(post.createdAt)}</p>
+        <h2 className='font-bold text-3xl'>{post.title}</h2>
+        <p className='opacity-90'>{post.excerpt}</p>
       </div>
     </Link>
   ) : (
