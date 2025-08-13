d35f59e4dfcac11ee0e979e70709d066f50c7946
Lucas Rosa <lucasmetron@gmail.com>
2025-08-13 10:54:50 -0300

    notfound next

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,13 +1,23 @@
-import { findBySlugCached } from '@/lib/posts/queries';
 import React from 'react';
+import { notFound } from 'next/navigation';
 
-type PostType = {
+import { findBySlugCached } from '@/lib/posts/queries';
+import { PostType } from '@/Types/PostType';
+
+type PostPageType = {
   params: Promise<{ slug: string }>;
 };
 
-const Post = async ({ params }: PostType) => {
+const Post = async ({ params }: PostPageType) => {
   const { slug } = await params;
-  const post = await findBySlugCached(slug);
+
+  let post: null | PostType = null;
+
+  try {
+    post = await findBySlugCached(slug);
+  } catch {
+    notFound();
+  }
 
   return (
     <div>
