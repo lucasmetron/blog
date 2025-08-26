ba382ed8eb61c224fd3e899369e38ab39d89c1a4
Lucas Rosa <lucasmetron@gmail.com>
2025-08-26 14:05:22 -0300

    apagando funçlões

--- src/db/drizzle/test.ts
+++ src/db/drizzle/test.ts
@@ -1,10 +1,10 @@
-import { eq } from 'drizzle-orm';
-import { drizzleDb } from '.';
-import { postsTable } from './schemas';
+// import { eq } from 'drizzle-orm';
+// import { drizzleDb } from '.';
+// import { postsTable } from './schemas';
 
-(async () => {
-  await drizzleDb
-    .update(postsTable)
-    .set({ title: 'Dicas para manter a saúde mental em dia' })
-    .where(eq(postsTable.slug, 'dicas-para-manter-a-saude-mental-em-dia'));
-})();
+// (async () => {
+//   await drizzleDb
+//     .update(postsTable)
+//     .set({ title: 'Dicas para manter a saúde mental em dia' })
+//     .where(eq(postsTable.slug, 'dicas-para-manter-a-saude-mental-em-dia'));
+// })();
