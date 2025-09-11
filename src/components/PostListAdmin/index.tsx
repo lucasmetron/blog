8462de0c97427efe1a523afea5587b7f5163b870
Lucas Rosa <lucasmetron@gmail.com>
2025-09-11 14:02:48 -0300

    feature: lista

--- src/components/PostListAdmin/index.tsx
+++ src/components/PostListAdmin/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import clsx from 'clsx';
+import { Trash } from 'lucide-react';
+import Link from 'next/link';
 
 import { findAllPostsAdmin } from '@/lib/posts/queries/admin';
 
@@ -6,11 +9,43 @@ const PostListAdmin = async () => {
   const listPost = await findAllPostsAdmin();
 
   return (
-    <div>
+    <div
+      className={clsx(
+        'w-full',
+        'h-[70vh]',
+        'pt-3',
+        'overflow-auto',
+        'flex',
+        'flex-col',
+        'items-center',
+        'justify-start',
+        'gap-3',
+      )}
+    >
       {listPost.map(post => (
-        <p key={post.id}>{post.title}</p>
+        <Link
+          href={`/admin/post/${post.id}`}
+          key={post.id}
+          className={clsx(
+            'w-full',
+            'py-2',
+            'px-2',
+            'flex',
+            'items-center',
+            'justify-between',
+            'cursor-pointer',
+            'rounded-[3px]',
+            post.published ? 'bg-transparent' : 'bg-gray-300',
+          )}
+        >
+          <p className='text-gray-900 text-md'>{post.title}</p>
+          {!post.published && (
+            <p className='text-sm italic font-thin'>{`(Não publicado)`}</p>
+          )}
+
+          <Trash color='red' />
+        </Link>
       ))}
-      <h1>{listPost[0].title}</h1>
     </div>
   );
 };
